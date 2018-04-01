fb149df15e9b3a911dc6891e0200508a8dd06227
Igor Shishkin <me@teran.ru>
2018-04-01 23:11:02 +0300

    Add ability to run custom scripts before running php5-fpm/nginx
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,14 @@
 
 set -euo pipefail
 
+# Run scripts to configure/prepare environment in runtime
+if [[ -d "/entrypoint.d" ]] ; then
+  for script in /entrypoint.d/* ; do
+    if [[ -x "${script}" ]] ; then
+      "${script}"
+    fi
+  done
+fi
+
 # Run supervisord
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
